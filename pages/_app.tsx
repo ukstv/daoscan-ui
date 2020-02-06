6869d0e64dabe94fe96e2c6ac5d39fc6b1b775d1
Sergey Ukustov <sergey.ukustov@machinomy.com>
2020-02-06 20:17:45 +0400

    Add favicon and icons stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,23 @@
 import * as React from "react";
 import "graphiql/graphiql.css";
 import { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta name="apple-mobile-web-app-title" content="Daoscan" />
+        <meta name="description" content="Daoscan - DAO Indexer" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/images/logo192.png" />
+        <link rel="icon" href="/images/logo192.png" sizes="192x192" />
+        <link rel="icon" href="/images/logo512.png" sizes="512x512" />
+      </Head>
       <Component {...pageProps} />
       <script data-goatcounter="https://daoscan.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
     </>
