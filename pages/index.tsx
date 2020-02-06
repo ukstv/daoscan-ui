1c70dfb9dce2ad42e9645429f654f36eca574e42
Sergey Ukustov <sergey.ukustov@machinomy.com>
2020-02-06 12:01:02 +0400

    Basic Nextjs stuff

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+import React from "react";
+
+const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => <h1>Hello world! - user agent: {userAgent}</h1>;
+
+Home.getInitialProps = async ({ req }) => {
+  const userAgent = req ? req.headers["user-agent"] || "" : navigator.userAgent;
+  return { userAgent };
+};
+
+export default Home;
