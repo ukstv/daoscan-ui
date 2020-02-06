fc4b58501b0d9deeecd6ce2b9342745a4baae813
Sergey Ukustov <sergey.ukustov@machinomy.com>
2020-02-06 16:33:38 +0400

    Add goatscanner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,12 @@ import "graphiql/graphiql.css";
 import { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <script data-goatcounter="https://daoscan.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
+    </>
+  );
 }
 
 export default MyApp;
