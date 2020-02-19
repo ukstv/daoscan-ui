b2ada0377083097567faed6301b49a4ef23e216c
Sergey Ukustov <sergey.ukustov@machinomy.com>
2020-02-19 14:39:42 +0400

    Logo as link goes to the front page

--- components/logo.component.tsx
+++ components/logo.component.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { Box } from "@theme-ui/components";
+import Link from "next/link";
+import styled from "@emotion/styled";
+
+const Anchor = styled.a`
+  border-bottom: none;
+  cursor: pointer;
+`;
 
 export function Logo() {
   return (
     <Box>
-      <span role={"img"}>🖇</span>️ daoscan
+      <Link href={"/"}>
+        <Anchor>
+          <span role={"img"}>🖇</span>️ daoscan
+        </Anchor>
+      </Link>
     </Box>
   );
 }
