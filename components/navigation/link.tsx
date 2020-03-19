30c27f47747232a144b90135d981488d7f6248fd
Sergey Ukustov <sergey.ukustov@machinomy.com>
2020-03-19 18:58:18 +0400

    Do not pass className to Next Link

--- components/navigation/link.tsx
+++ components/navigation/link.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-
+import _ from "lodash";
 import { NavLink } from "@theme-ui/components";
 import { default as NextLink, LinkProps as NextLinkProps } from "next/link";
 
-export type LinkProps = NextLinkProps & { className?: string, target?: '_blank' };
+export type LinkProps = NextLinkProps & { className?: string; target?: "_blank" };
 
 export function Link(props: React.PropsWithChildren<LinkProps>) {
+  const nextProps = _.omit(props, ["className"]);
   return (
-    <NextLink {...props} passHref={true}>
+    <NextLink {...nextProps} passHref={true}>
       <NavLink className={props.className}>{props.children}</NavLink>
     </NextLink>
   );
